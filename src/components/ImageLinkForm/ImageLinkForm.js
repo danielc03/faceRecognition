b96bb6067cbc5ac5e498d63261c0953b24e2add4
c-daniel14 <cojocarugeani@yahoo.com>
2018-05-28 14:03:26 +0100

    design change

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -12,7 +12,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 				<div className="center form pa4 br3 shadow-5">
 					<input className="f4 pa2 w-70 center" type="tex" onChange={onInputChange}/>
 					<button 
-					className="w-30 grow f4 link ph3 br2 pv2 dib white bg-light-purple"
+					className="w-30 grow f4 link ph3 br2 pv2 dib white bg-light-blue"
 					onClick={onButtonSubmit}>Detect</button>
 				</div>
 			</div>
